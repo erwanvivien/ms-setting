916bd9411e13c83498e9d023b2e3029074d40b9f
erwan.vivien <erwan.vivien@epita.fr>
2021-11-15 13:36:13 +0100

    [feature] Added tilt on select

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
 document.getElementById('copy').onclick = function (e) {
     navigator.clipboard.writeText("ms-settings:")
 }
+
+document.querySelectorAll('.category').forEach((category) => {
+    const width = category.clientWidth;
+    const height = category.clientHeight;
+
+    category.onmousedown = (e) => {
+        /// Should range from -1 to 1
+        const relativeMiddleX = ((e.offsetX - width / 2) / width) * 2
+        /// Should range from -1 to 1
+        const relativeMiddleY = ((e.offsetY - height / 2) / height) * 2
+        const deg = 10;
+
+        category.style.transform =
+            ["perspective(1000px)",
+                `rotateX(${deg * -relativeMiddleY}deg)`,
+                `rotateY(${deg * relativeMiddleX}deg)`,
+                `rotateZ(${deg / 5 * relativeMiddleX * relativeMiddleY}deg)`,
+            ].join(' ');
+    }
+
+    category.onmouseup = (e) => {
+        category.style.transform = '';
+    }
+
+    category.onmouseleave = (e) => {
+        category.style.transform = '';
+    }
+})
