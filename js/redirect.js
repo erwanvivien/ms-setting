ee33002b22fa3e663996ef039ccf8d6dd3fdc793
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-01-04 14:21:41 +0100

    fix: XSS on #redirect

--- js/redirect.js
+++ js/redirect.js
@@ -1,5 +1,233 @@
-const hash = window.location.hash;
+const possibleValues = [
+    // Settings home page
+    "ms-settings:", // Settings home page
+    // System
+    "ms-settings:display", // Display
+    "ms-settings:nightlight", // Night light settings
+    "ms-settings:display-advanced", // Advanced scaling settings
+    "ms-settings-connectabledevices:devicediscovery", // Connect to a wireless display
+    "ms-settings:display-advancedgraphics", // Graphics settings
+    "ms-settings:screenrotation", // Display orientation
+    "ms-settings:sound", // Sound
+    "ms-settings:sound-devices", // Manage sound devices
+    "ms-settings:apps-volume", // App volume and device preferences
+    "ms-settings:notifications", // Notifications & actions
+    "ms-settings:quiethours", // Focus assist
+    "ms-settings:quietmomentshome", // Focus assist
+    "ms-settings:quietmomentsscheduled", // During these hours
+    "ms-settings:quietmomentspresentation", // Duplicating my display (When I'm duplicating my display)
+    "ms-settings:quietmomentsgame", // Playing a game full screen (When I'm playing a game)
+    "ms-settings:powersleep", // Power & sleep
+    "ms-settings:batterysaver", // Battery
+    "ms-settings:batterysaver-usagedetails", // See which apps are affecting your battery life
+    "ms-settings:batterysaver-settings", // Battery Saver settings
+    "ms-settings:storagesense", // Storage
+    "ms-settings:storagepolicies", // Configure Storage Sense or run it now
+    "ms-settings:savelocations", // Change where new content is saved
+    "ms-settings:tabletmode", // Tablet mode
+    "ms-settings:multitasking", // Multitasking
+    "ms-settings:project", // Projecting to this PC
+    "ms-settings:crossdevice", // Shared experiences
+    "ms-settings:clipboard", // Clipboard
+    "ms-settings:remotedesktop", // Remote Desktop
+    "ms-settings:deviceencryption", // Device Encryption (where available)
+    "ms-settings:about", // About
+    // Devices
+    "ms-settings:bluetooth", // Bluetooth & other devices
+    "ms-settings:connecteddevices", // Bluetooth & other devices
+    "ms-settings:printers", // Printers & scanners
+    "ms-settings:mousetouchpad", // Mouse
+    "ms-settings:devices-touchpad", // Touchpad
+    "ms-settings:typing", // Typing
+    "ms-settings:devicestyping-hwkbtextsuggestions", // Hardware keyboard - Text suggestions
+    "ms-settings:wheel", // Wheel (where available)
+    "ms-settings:pen", // Pen & Windows Ink
+    "ms-settings:autoplay", // AutoPlay
+    "ms-settings:usb", // USB
+    // Phone
+    "ms-settings:mobile-devices", // Phone
+    "ms-settings:mobile-devices-addphone", // Add a phone
+    "ms-settings:mobile-devices-addphone-direct", // Your Phone (opens app)
+    // Network & Internet
+    "ms-settings:network", // Network & Internet
+    "ms-settings:network-status", // Status
+    "ms-availablenetworks:", // Show available networks
+    "ms-settings:network-cellular", // Cellular & SIM
+    "ms-settings:network-wifi", // Wi-Fi
+    "ms-availablenetworks:", // Show available networks
+    "ms-settings:network-wifisettings", // Manage known networks
+    "ms-settings:network-wificalling", // Wi-Fi Calling
+    "ms-settings:network-ethernet", // Ethernet
+    "ms-settings:network-dialup", // Dial-up
+    "ms-settings:network-directaccess", // DirectAccess (where available)
+    "ms-settings:network-vpn", // VPN
+    "ms-settings:network-airplanemode", // Airplane mode
+    "ms-settings:proximity", // Airplane mode
+    "ms-settings:network-mobilehotspot", // Mobile hotspot
+    "ms-settings:nfctransactions", // NFC
+    "ms-settings:datausage", // Data usage
+    "ms-settings:network-proxy", // Proxy
+    // Personalization
+    "ms-settings:personalization", // Personalization
+    "ms-settings:personalization-background", // Background
+    "ms-settings:personalization-colors", // Colors
+    "ms-settings:colors", // Colors
+    "ms-settings:lockscreen", // Lock screen
+    "ms-settings:themes", // Themes
+    "ms-settings:fonts", // Fonts (build 17083+)
+    "ms-settings:personalization-start", // Start
+    "ms-settings:personalization-start-places", // Choose which folders appear on Start
+    "ms-settings:taskbar", // Taskbar
+    // Apps
+    "ms-settings:appsfeatures", // Apps & features
+    "ms-settings:appsfeatures-app", // Apps & features
+    "ms-settings:optionalfeatures", // Manage optional features
+    "ms-settings:defaultapps", // Default apps
+    "ms-settings:maps", // Offline maps
+    "ms-settings:maps-downloadmaps", // Download maps
+    "ms-settings:appsforwebsites", // Apps for websites
+    "ms-settings:videoplayback", // Video playback
+    "ms-settings:startupapps", // Startup
+    // Accounts
+    "ms-settings:yourinfo", // Your info
+    "ms-settings:emailandaccounts", // Email & accounts
+    "ms-settings:signinoptions", // Sign-in options
+    "ms-settings:signinoptions-launchfaceenrollment", // Windows Hello face setup
+    "ms-settings:signinoptions-launchfingerprintenrollment", // Windows Hello fingerprint setup
+    "ms-settings:signinoptions-launchsecuritykeyenrollment", // Security Key setup
+    "ms-settings:signinoptions-dynamiclock", // Dynamic Lock
+    "ms-settings:workplace", // Access work or school
+    "ms-settings:otherusers", // Family & other people
+    "ms-settings:assignedaccess", // Set up a kiosk
+    "ms-settings:sync", // Sync your settings
+    // Time & language
+    "ms-settings:dateandtime", // Date & time
+    "ms-settings:regionformatting", // Region
+    "ms-settings:regionlanguage-jpnime", // Japan IME settings (where available)
+    "ms-settings:regionlanguage-chsime-pinyin", // Pinyin IME settings (where available)
+    "ms-settings:regionlanguage-chsime-wubi", // Wubi IME settings (where available)
+    "ms-settings:regionlanguage-korime", // Korea IME settings (where available)
+    "ms-settings:regionlanguage", // Language
+    "ms-settings:regionlanguage-languageoptions", // Language
+    "ms-settings:regionlanguage-setdisplaylanguage", // Windows Display language
+    "ms-settings:regionlanguage-adddisplaylanguage", // Add Display language
+    "ms-settings:keyboard", // Keyboard (removed in build 17083+)
+    "ms-settings:speech", // Speech
+    // Gaming
+    "ms-settings:gaming-gamebar", // Game bar
+    "ms-settings:gaming-gamedvr", // Captures
+    "ms-settings:gaming-broadcasting", // Broadcasting
+    "ms-settings:gaming-gamemode", // Game Mode
+    "ms-settings:gaming-trueplay", // TruePlay (removed in version 1809+)
+    "ms-settings:gaming-xboxnetworking", // Xbox Networking
+    // Extras
+    "ms-settings:extras", // Extras (available only when Settings app extensions installed)
+    // Ease of Access
+    "ms-settings:easeofaccess-display", // Display
+    "ms-settings:easeofaccess-cursorandpointersize", // Mouse Pointer
+    "ms-settings:easeofaccess-cursor", // Text Cursor
+    "ms-settings:easeofaccess-magnifier", // Magnifier
+    "ms-settings:easeofaccess-colorfilter", // Color Filters
+    "ms-settings:easeofaccess-colorfilter-adaptivecolorlink", // Adaptive Color Filters Link
+    "ms-settings:easeofaccess-colorfilter-bluelightlink", // Night Light Link
+    "ms-settings:easeofaccess-highcontrast", // High Contrast
+    "ms-settings:easeofaccess-narrator", // Narrator
+    "ms-settings:easeofaccess-narrator-isautostartenabled", // Start Narratorafter sign-in for me
+    "ms-settings:easeofaccess-audio", // Audio
+    "ms-settings:easeofaccess-closedcaptioning", // Closed captions
+    "ms-settings:easeofaccess-speechrecognition", // Speech
+    "ms-settings:easeofaccess-keyboard", // Keyboard
+    "ms-settings:easeofaccess-mouse", // Mouse
+    "ms-settings:easeofaccess-eyecontrol", // Eye Control
+    "ms-settings:easeofaccess-otheroptions", // Other options (removed in version 1809+)
+    // Search (version 1903+)
+    "ms-settings:search-permissions", // Permissions & history
+    "ms-settings:cortana-windowssearch", // Searching Windows
+    "ms-settings:search-moredetails", // More details
+    // Cortana
+    "ms-settings:cortana", // Cortana
+    "ms-settings:cortana-talktocortana", // Talk to Cortana
+    "ms-settings:cortana-permissions", // Permissions
+    "ms-settings:cortana-moredetails", // More details
+    // Privacy
+    "ms-settings:privacy", // General
+    "ms-settings:privacy-speech", // Speech
+    "ms-settings:privacy-speechtyping", // Inking & typing personalization
+    "ms-settings:privacy-feedback", // Diagnostics & feedback
+    "ms-settings:privacy-feedback-telemetryviewergroup", // View Diagnostic Data
+    "ms-settings:privacy-activityhistory", // Activity history (build 17040+)
+    "ms-settings:privacy-location", // Location
+    "ms-settings:privacy-webcam", // Camera
+    "ms-settings:privacy-microphone", // Microphone
+    "ms-settings:privacy-voiceactivation", // Voice activation
+    "ms-settings:privacy-notifications", // Notifications
+    "ms-settings:privacy-accountinfo", // Account info
+    "ms-settings:privacy-contacts", // Contacts
+    "ms-settings:privacy-calendar", // Calendar
+    "ms-settings:privacy-phonecalls", // Phone calls (removed in version 1809+)
+    "ms-settings:privacy-callhistory", // Call history
+    "ms-settings:privacy-email", // Email
+    "ms-settings:privacy-eyetracker", // Eye tracker (requires eyetracker hardware)
+    "ms-settings:privacy-tasks", // Tasks
+    "ms-settings:privacy-messaging", // Messaging
+    "ms-settings:privacy-radios", // Radios
+    "ms-settings:privacy-customdevices", // Other devices
+    "ms-settings:privacy-backgroundapps", // Background apps
+    "ms-settings:privacy-appdiagnostics", // App diagnostics
+    "ms-settings:privacy-automaticfiledownloads", // Automatic file downloads
+    "ms-settings:privacy-documents", // Documents
+    "ms-settings:privacy-pictures", // Pictures
+    "ms-settings:privacy-documents", // Videos
+    "ms-settings:privacy-broadfilesystemaccess", // File system
+    // Update & security
+    "ms-settings:windowsupdate", // Windows Update
+    "ms-settings:windowsupdate-action", // Check for updates
+    "ms-settings:windowsupdate-history", // View update history
+    "ms-settings:windowsupdate-restartoptions", // Restart options
+    "ms-settings:windowsupdate-options", // Advanced options
+    "ms-settings:windowsupdate-activehours", // Change active hours
+    "ms-settings:windowsupdate-optionalupdates", // Optional updates
+    "ms-settings:windowsupdate-seekerondemand", // Optional updates
+    "ms-settings:delivery-optimization", // Delivery Optimization
+    "ms-settings:windowsdefender", // Windows Security / Windows Defender
+    "windowsdefender:", // Open Windows Security
+    "ms-settings:backup", // Backup
+    "ms-settings:troubleshoot", // Troubleshoot
+    "ms-settings:recovery", // Recovery
+    "ms-settings:activation", // Activation
+    "ms-settings:findmydevice", // Find My Device
+    "ms-settings:developers", // For developers
+    "ms-settings:windowsinsider", // Windows Insider Program
+    "ms-settings:windowsinsider-optin", // Windows Insider Program
+    // Mixed reality
+    "ms-settings:holographic", // Mixed reality
+    "ms-settings:holographic-audio", // Audio and speech
+    "ms-settings:privacy-holographic-environment", // Environment
+    "ms-settings:holographic-headset", // Headset display
+    "ms-settings:holographic-management", // Uninstall
+    // Surface Hub
+    "ms-settings:surfacehub-accounts", // Accounts
+    "ms-settings:surfacehub-calling", // Team Conferencing
+    "ms-settings:surfacehub-devicemanagenent", // Team device management
+    "ms-settings:surfacehub-sessioncleanup", // Session cleanup
+    "ms-settings:surfacehub-welcome", // Welcome screen
+];
 
-if (hash) {
-    window.location = (hash.slice(1));
+const checkHash = (hash) => {
+  if (!hash || !hash.slice(1)) return;
+
+  const hashValue = hash.slice(1);
+  const hashIndex = window.location.href.lastIndexOf(hash);
+  if (!possibleValues.includes(hashValue)) {
+    window.location = window.location.href.slice(0, hashIndex);
+  }
+
+  try {
+    window.open(hashValue, '_blank').focus();
+  } catch (error) {
+    window.location = hashValue;
+  }
 }
+
+window.onload = () => checkHash(window.location.hash);
+window.onhashchange = (e) => checkHash(window.location.hash);
