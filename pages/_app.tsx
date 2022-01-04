ed09d9a08106b9b4a1a7e3be516acd5b7c840f3c
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-01-04 16:18:00 +0100

    fix: couldn't use some settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,13 @@ import type { AppProps } from "next/app";
 
 import MS_SETTINGS from "../src/ms-settings";
 
-const MS_SETTINGS_SUBSTR = MS_SETTINGS.map((setting) => setting.substring(12));
-
 function MyApp({ Component, pageProps, router }: AppProps) {
   if (
     router.query &&
-    typeof router.query["ms-settings"] === "string" &&
-    MS_SETTINGS_SUBSTR.includes(router.query["ms-settings"])
+    typeof router.query["setting"] === "string" &&
+    MS_SETTINGS.includes(router.query["setting"])
   ) {
-    console.log(router.query["ms-settings"]);
+    console.log(router.query["setting"]);
   }
 
   return <Component {...pageProps} />;
