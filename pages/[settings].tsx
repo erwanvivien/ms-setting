73c9d24aab9300e0783e3eaead840677d1640ad2
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-01-05 12:38:38 +0100

    fix: added all settings for every pages

--- pages/[settings].tsx
+++ pages/[settings].tsx
@@ -3,11 +3,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Settings.module.css";
 
-import SettingPanel from "../components/SettingPanel";
 import SettingMore from "../components/SettingMore";
 import SettingBuilder from "../components/SettingBuilder";
+import SettingPanel from "../components/SettingPanel";
+import { useRouter } from "next/router";
 
 const Template: NextPage = () => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -17,10 +20,10 @@ const Template: NextPage = () => {
 
       <div style={{ display: "flex", flexDirection: "row" }}>
         <header className={styles.header}>
-          <SettingPanel />
+          <SettingPanel router={router} />
         </header>
         <main className={styles.main}>
-          <SettingBuilder title="Bluetooth and devices" />
+          <SettingBuilder router={router} />
         </main>
         <footer className={styles.footer}>
           <SettingMore />
