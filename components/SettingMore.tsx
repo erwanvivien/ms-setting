c725b10899848252ae1f1d5a8830b318613c875d
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-01-05 15:23:02 +0100

    feature: using builder for more panel

--- components/SettingMore.tsx
+++ components/SettingMore.tsx
@@ -2,32 +2,77 @@ import React from "react";
 import type { NextPage } from "next";
 import styles from "../styles/Settings.module.css";
 
+enum BuilderType {
+  Title,
+  Desc,
+  Link,
+}
+
+const BuilderMap: {
+  [k: string]: ({ value }: { value: string }) => JSX.Element;
+} = Object.fromEntries([
+  [
+    BuilderType.Title,
+    ({ value }: { value: string }) => (
+      <p className={styles.footer_title}>{value}</p>
+    ),
+  ],
+  [
+    BuilderType.Desc,
+    ({ value }: { value: string }) => (
+      <p className={styles.footer_description}>{value}</p>
+    ),
+  ],
+  [
+    BuilderType.Link,
+    ({ value }: { value: string }) => (
+      <p className={styles.footer_link}>{value}</p>
+    ),
+  ],
+]);
+
+type SettingMoreBuilder = {
+  type: BuilderType;
+  value: string;
+}[][];
+
+const more: SettingMoreBuilder = [
+  [
+    { type: BuilderType.Title, value: `Turn on Bluetooth even faster` },
+    {
+      type: BuilderType.Desc,
+      value: `To turn on Bluetooth without opening Settings, open action center, and
+      then select Bluetooth icon. Do the same to turn it off when you want.`,
+    },
+    { type: BuilderType.Link, value: `Get more info about Bluetooth` },
+  ],
+  [
+    { type: BuilderType.Title, value: "Related settings" },
+    { type: BuilderType.Link, value: "Devices and printers" },
+    { type: BuilderType.Link, value: "Sound settings" },
+    { type: BuilderType.Link, value: "Display settings" },
+    { type: BuilderType.Link, value: "More Bluetooth options" },
+    { type: BuilderType.Link, value: "Send or receive files via Bluetooth" },
+  ],
+  [
+    { type: BuilderType.Title, value: "Have a question ?" },
+    { type: BuilderType.Link, value: "Share files over Bluetooth" },
+    { type: BuilderType.Link, value: "Reinstall Bluetooth drivers" },
+    { type: BuilderType.Link, value: "Fix Bluetooth connections" },
+    { type: BuilderType.Link, value: "Get help" },
+  ],
+];
+
 const SettingMore: NextPage = () => (
   <>
-    <div className={styles.footer_container}>
-      <p className={styles.footer_title}>Turn on Bluetooth even faster</p>
-      <p className={styles.footer_description}>
-        To turn on Bluetooth without opening Settings, open action center, and
-        then select Bluetooth icon. Do the same to turn it off when you want.
-      </p>
-      <p className={styles.footer_link}>Get more info about Bluetooth</p>
-    </div>
-    <div className={styles.footer_container}>
-      <p className={styles.footer_title}>Related settings</p>
-      <p className={styles.footer_link}>Devices and printers</p>
-      <p className={styles.footer_link}>Sound settings</p>
-      <p className={styles.footer_link}>Display settings</p>
-      <p className={styles.footer_link}>More Bluetooth options</p>
-      <p className={styles.footer_link}>Send or receive files via Bluetooth</p>
-    </div>
-
-    <div className={styles.footer_container}>
-      <p className={styles.footer_title}>Have a question ?</p>
-      <p className={styles.footer_link}>Share files over Bluetooth</p>
-      <p className={styles.footer_link}>Reinstall Bluetooth drivers</p>
-      <p className={styles.footer_link}>Fix Bluetooth connections</p>
-      <p className={styles.footer_link}>Get help</p>
-    </div>
+    {more.map((category, idx) => (
+      <div className={styles.footer_container} key={idx}>
+        {category.map((item, idx) => {
+          const Block = BuilderMap[item.type];
+          return <Block {...item} key={idx} />;
+        })}
+      </div>
+    ))}
   </>
 );
 
