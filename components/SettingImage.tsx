66cc3132eb4747a2f7e63b08d883745a07e4b089
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-01-08 15:50:20 +0100

    fix: remove responsive from image

--- components/SettingImage.tsx
+++ components/SettingImage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import stylesSetting from "../styles/Settings.module.css";
+import styles from "../styles/Settings.module.css";
 
 type SettingImageProps = {
   page: string;
@@ -10,13 +10,16 @@ type SettingImageProps = {
 
 const SettingImage = ({ page, image }: SettingImageProps) => {
   return (
-    <div className={stylesSetting.setting_builder_container}>
+    <div className={styles.setting_builder_container}>
       {image && (
         <Image
           src={image}
           alt={`Setting page for ${page}`}
           // layout="fill"
+          width={1250}
+          height={1010}
           quality={95}
+          layout="fixed"
         />
       )}
     </div>
