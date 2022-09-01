5c499e769f25aa1d8552b2b922757b366d8ff9b7
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-09-01 20:21:28 +0200

    fix: cache all pages

--- next.config.js
+++ next.config.js
@@ -7,4 +7,17 @@ module.exports = {
   eslint: {
     dirs: ['pages', 'src', 'components'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=604800',
+          },
+        ],
+      },
+    ]
+  },
 }
